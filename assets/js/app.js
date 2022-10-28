c37fc675a9046d6b75ddd003928bc08b7c95555a
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-28 13:35:54 +0200

    Milestone 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,36 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            tasks: [
+                {
+                    text: 'Fare esercizio del pomeriggio',
+                    done: true,
+                },
+                {
+                    text: 'Lavare i piatti',
+                    done: false,
+                },
+                {
+                    text: 'Andare a correre',
+                    done: false,
+                },
+                {
+                    text: 'Imparare Tedesco',
+                    done: true,
+                },
+                {
+                    text: 'Trovare un lavoro',
+                    done: false,
+                },
+                {
+                    text: 'Imparare php',
+                    done: false,
+                },
+                {
+                    text: 'Fare la patente',
+                    done: true,
+                },
+            ],
         }
     }
 }).mount('#app')
